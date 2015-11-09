4c77e148e5ae54dfa4cbd9cb132a81f78a6edbdf
João Santos <joao.santos@zalando.de>
2015-11-09 20:19:03 +0100

    Add Python 3.5 Dockerfile

---
+++ python35/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VER=$(cat VERSION)
+
+docker build -t zalando/python:$VER .
+
+SQUASH_PATH=$(which docker-squash)
+
+if [ -z "$SQUASH_PATH" ]; then
+    # install Docker Squash from https://github.com/jwilder/docker-squash
+    TGZ=docker-squash-linux-amd64-v0.1.0.tar.gz
+    wget https://github.com/jwilder/docker-squash/releases/download/v0.1.0/$TGZ
+    sudo tar -C /usr/local/bin -xzvf $TGZ
+    rm $TGZ
+fi
+
+# NOTE: disable docker-squash as it now breaks the layer tar format (reason unknown)
+#echo 'Squashing the base image to save space..'
+#docker save zalando/python:$VER | sudo docker-squash -t zalando/python:$VER | docker load
